4eb3c8a28398bbb3f941481a3b54ea933b833a49
gianayang <gianayang31@gmail.com>
2021-07-27 11:16:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,180 @@
+import React, {Component} from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Register from './components/Register/Register';
+import Signin from './components/Signin/Signin';
+import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '0a409bc202f74f10af0cb863826b31d0'
+});
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+      imageUrl:'',
+      box:{},
+      route: 'signin',
+      isSignedIn: false,
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height)
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit= () =>{
+    this.setState({imageUrl: this.state.input})
+    app.models
+    .predict(
+      'f76196b43bbd45c99b4f3cd8e8b40a8a', 
+      this.state.input )
+    .then( response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err=> console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const {isSignedIn, imageUrl, route, box} = this.state;
+      return (
+        <div className="App">
+          <Particles
+            className='Particles'
+            id="tsparticles"
+            init={this.particlesInit}
+            loaded={this.particlesLoaded}
+            options={{
+              background: {
+                color: {
+                  value: "#00000",
+                },
+              },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#FFC0CB",
+            },
+            links: {
+              color: "#FFC0CB",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 8,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+          
+      {route === 'home' 
+      ?<div>
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={box} imageUrl={imageUrl}/>
+      </div>
+        :(
+          route === 'signin'
+          ? <Signin onRouteChange={this.onRouteChange}/>
+          : <Register onRouteChange={this.onRouteChange}/>
+        )
+      }
+      </div>
+      );
+      }
+}
+
+export default App;
